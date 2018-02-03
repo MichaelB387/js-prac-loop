67bae1aaa57e043b8b6734a536cbd505a7e451c5
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-02-03 22:56:01 +1100

    refactored with function

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ var rgbColor;
 var i =0
 
 for (i = 0; i < 100; i++) { 
+    random()
+}
+
+
+function random() {
     red = Math.floor(Math.random() * 256 );
     green = Math.floor(Math.random() * 256 );
     blue = Math.floor(Math.random() * 256 );
     rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     html += '<div style="background-color:' + rgbColor + '"></div>';
 }
-
 
 document.write(html);
